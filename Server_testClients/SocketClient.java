445745a499eda8990d73882c3147d176ab8a51cd
Zac <zkornas@uw.edu>
2023-06-01 02:29:09 -0700

    Updating client to allow continuous connection

--- Server_testClients/SocketClient.java
+++ Server_testClients/SocketClient.java
@@ -5,25 +5,30 @@ public class SocketClient {
     public static void main(String... args) {
         String hostname = args[0];
         int port = Integer.parseInt(args[1]);
-        String req = "";
-        for (int i = 2; i < args.length; i++) {
-            req += args[i] + " ";
-        }
 
         try (Socket s = new Socket(hostname, port)) {
             s.setSoTimeout(15000);
 
             OutputStream out = s.getOutputStream();
-            out.write(req.getBytes());
-
             InputStream in = s.getInputStream();
-            StringBuilder response = new StringBuilder();
-            InputStreamReader reader = new InputStreamReader(in, "ASCII");
-            for (int c = reader.read(); c != -1; c = reader.read()) {
-                response.append((char) c);
-            }
 
-            System.out.println("Server response: " + response);
+            BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+            String userInput;
+
+            do {
+                System.out.print("Enter a command (or 'exit' to quit): ");
+                userInput = userInputReader.readLine();
+
+                out.write(userInput.getBytes());
+
+                StringBuilder response = new StringBuilder();
+                InputStreamReader reader = new InputStreamReader(in, "ASCII");
+                for (int c = reader.read(); c != -1; c = reader.read()) {
+                    response.append((char) c);
+                }
+
+                System.out.println("Server response: " + response);
+            } while (!userInput.equals("exit"));
 
         } catch (IOException ex) {
             System.err.println(ex);
